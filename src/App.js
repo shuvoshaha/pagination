ccd064e98d1f647ddb024a7e75725c2a1a695a81
shuvoshaha <shaha460@gmail.com>
2020-12-16 12:46:49 +0600

    Caculation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Pagination from './pagination'
 
 const App = () => {
  const [post, setPost ] = useState([]);
  const [loading, setLoading] = useState(false);
- const [currentPagem, setCurrentPage] = useState(1)
- const [postPerPage, setPostPerPage] = useState(10)
+ const [currentPage, setCurrentPage] = useState(1)
+ const [postPerPage, setPostPerPage] = useState(15)
  
+ // Get the value with RESTful API
  useEffect(() =>{
    const fetchData = async ()=>{
      setLoading(true);
@@ -19,9 +21,17 @@ const App = () => {
 
  }, [])
  console.log(post)
+
+ // Pagination Calculation
+ const indexOfLastPage = currentPage * postPerPage;
+ const indexOfFristPage = indexOfLastPage - postPerPage;
+ const currentPost = post.slice(indexOfFristPage, indexOfLastPage);
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="container">
+      <h1>Blog</h1>
+      <div className="post-title">
+        <Pagination posts={currentPost} loading={loading} />
+      </div>
     </div>
   );
 }
