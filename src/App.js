82998709291cd17e4de617a2bedbf9e2f7489326
shuvoshaha <shaha460@gmail.com>
2020-12-16 12:08:30 +0600

    Initialize all state for pagination

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+ const [post, setPost ] = useState([]);
+ const [loading, setLoading] = useState(false);
+ const [currentPagem, setCurrentPage] = useState(1)
+ const [postPerPage, setPostPerPage] = useState(10)
+ 
+ useEffect(() =>{
+   const fetchData = async ()=>{
+     setLoading(true);
+     const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+     setPost(res.data);
+     setLoading(false);
+   }
+   fetchData()
+
+ }, [])
+ console.log(post)
   return (
     <div className="App">
       <h1>Hello World</h1>
